89c605940555c277901255a14d1364aa2bdbfd20
Bob <lapizjusoh@yahoo.com>
2025-01-31 14:27:59 +0800

    Render initial display

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,31 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>
-    </>
+    <div className='body'>
+      <header>
+        <div className='container'>
+          <h1>Genshin DB</h1>
+          <nav>
+            <ul>
+              <li><button>Home</button></li>
+              <li><button>Characters</button></li>
+              <li><button>Contact</button></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <div className='container'>
+          Main
+        </div>
+      </main>
+      <footer>
+        <div className='container'>
+          <p>Copyright &copy; Lapiz Jusoh 2025-2025</p>
+          <p>Genshin API Data obtained from </p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
